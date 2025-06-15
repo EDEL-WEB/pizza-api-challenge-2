8a0b7206a14ffa42acaec2a8e9c0ca369591faa8
edel <edel.omondi@student.moringaschool.com>
2025-06-15 12:14:24 +0300

    connected flask app to postgres db

---
+++ server/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from .config import Config
+from .models import db
+from flask_migrate import Migrate
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    # Initialize extensions
+    db.init_app(app)
+    Migrate(app, db)
+
+    return app
