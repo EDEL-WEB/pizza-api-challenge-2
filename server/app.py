b5464ebc0b98584c840ba80516ff949823b1e2fb
edel <edel.omondi@student.moringaschool.com>
2025-06-15 12:31:49 +0300

    corrected importation before running migration

--- server/app.py
+++ server/app.py
@@ -3,11 +3,15 @@ from .config import Config
 from .models import db
 from flask_migrate import Migrate
 
+
+from .models.pizza import Pizza
+from .models.restaurant import Restaurant
+from .models.restaurant_pizza import RestaurantPizza
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    # Initialize extensions
     db.init_app(app)
     Migrate(app, db)
 
